b0af7ea8631d645b77b9d9f208dbb8060f62bf02
unknown <a.afanasjev@LITE-FIN-03.itt.local>
2017-07-19 19:10:06 +0200

    create project

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Financial Audit',
+    'version': '0.1',
+    'category': '',
+    'description': """
+financial audit
+===================================================
+""",
+    'author': "(c) Artem Afanasev, 2017",
+    'e-mail': "@",
+    'website': "",
+    'depends': [],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+#    'post_init_hook': 'post_init',
+}
---
+++ models/audit_engagements.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, RedirectWarning, ValidationError
+#import odoo.addons.decimal_precision as dp
+
+class AuditEngagements(models.Model):
+    _name = "audit.engagements"
+#    _inherit = ['mail.thread']
+    _description = "Audit Engagements"
+    _order = "id"
+
+    name = fields.Char("Engagement", copy=False, required=True)
+    company_id = fields.Many2one('res.company', string='Company', change_default=True,
+        required=True, readonly=True, states={'draft': [('readonly', False)]},
+        default=lambda self: self.env.user.company_id.id )
+
+    _sql_constraints = [
+    ]
